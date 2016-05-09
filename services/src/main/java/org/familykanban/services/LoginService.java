e803549cb4c11596c52d04478741baf75cbfb5c0
paul.bernard <paul.bernard@asynchrony.com>
2016-05-08 20:57:55 -0500

    test

--- services/src/main/java/org/familykanban/services/LoginService.java
+++ services/src/main/java/org/familykanban/services/LoginService.java
@@ -1,7 +1,18 @@
 package org.familykanban.services;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Paul on 5/5/2016.
  */
+
+@RestController
 public class LoginService {
+
+    @RequestMapping("/")
+    public String getPage() {
+        return "";
+    }
+
 }
